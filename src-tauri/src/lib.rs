ecccfaed70d964dfad9389f44c1d07bb73d0078f
Dreamer528 <172515987+Dreamer528@users.noreply.github.com>
2026-04-20 17:22:32 +0700

    Stabilize Apple Silicon Tauri builds

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
             {
-                if let Some(window) = app.get_webview_window("main") {
+                use tauri::Manager;
+
+                if let Some(window) = _app.get_webview_window("main") {
                     window.open_devtools();
                 }
             }
